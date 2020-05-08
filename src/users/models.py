92d341890b7c305211f8f9923b7c75f3f0927fc2
Artem Belyakov <deboshit@yandex.ru>
2020-05-08 13:10:09 +0300

    remove redundant files

--- src/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Profile(models.Model):
-    """
-    Model for user representation
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
